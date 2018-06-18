eb72ffc698a1fb0c879f7c718c6a156b496037ed
NikFanton <guchenko.n@gmail.com>
2018-06-18 20:25:02 +0300

    added new loggers

---
+++ src/main/java/com/company/logger/impl/CombineEventLogger.java
@@ -0,0 +1,22 @@
+package com.company.logger.impl;
+
+import com.company.entity.Event;
+import com.company.logger.EventLogger;
+
+import java.util.List;
+
+public class CombinedEventLogger implements EventLogger {
+
+    private List<EventLogger> loggers;
+
+    public CombinedEventLogger(List<EventLogger> loggers) {
+        this.loggers = loggers;
+    }
+
+    @Override
+    public void logEvent(Event event) {
+        for (EventLogger logger : loggers) {
+            logger.logEvent(event);
+        }
+    }
+}
---
+++ src/main/java/com/company/logger/impl/FileEventLogger.java
@@ -0,0 +1,4 @@
+package com.company.logger.impl;
+
+public class FileEventLogger {
+}
